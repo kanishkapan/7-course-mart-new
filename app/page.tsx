6b71278234e384c55b5e02fbe56bafd1d61965cb
ayush69ji <ayushbirthray69@gmail.com>
2024-11-23 14:00:34 +0530

    feat:header_done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import Header from "@/components/header/Header";
+
 
 export default function Home() {
-  return <div>hi</div>;
+  return <>
+  
+  <Header/>
+  
+  </>;
 }
